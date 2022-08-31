edd7f5020fd0892ac7aad31076029a5ec71e4937
zirui <ziruiqiao@cmail.carleton.ca>
2022-08-31 14:01:38 -0400

    change apis and add util classes

---
+++ v1/1.0/pack/nart/src/main/java/com/nart/controller/UserController.java
@@ -0,0 +1,13 @@
+package com.nart.controller;/**
+* Copyright (c) 2008-2024: Zirui Qiao
+* Project: pack
+* @className: UserController
+* @Description: TODO 
+*
+* @version: v1.8.0
+* @author: ZIRUI QIAO
+* @date: 2022/8/31 13:25	
+*
+*/
+public class UserController {
+}
---
+++ v1/1.0/pack/nart/src/main/java/com/nart/util/Result.java
@@ -0,0 +1,13 @@
+package com.nart.util;/**
+* Copyright (c) 2008-2024: Zirui Qiao
+* Project: pack
+* @className: Result
+* @Description: TODO 
+*
+* @version: v1.8.0
+* @author: ZIRUI QIAO
+* @date: 2022/8/31 13:27	
+*
+*/
+public class Result {
+}
