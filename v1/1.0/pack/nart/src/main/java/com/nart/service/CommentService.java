c77527016976301fc8d50e4f193c7fecc143ad32
yunzhouliu1 <YunzhouLiu@cmail.carleton.ca>
2023-01-04 05:45:40 -0500

    test 和注释 进行中

--- v1/1.0/pack/nart/src/main/java/com/nart/service/CommentService.java
+++ v1/1.0/pack/nart/src/main/java/com/nart/service/CommentService.java
@@ -3,7 +3,18 @@ package com.nart.service;
 import com.nart.pojo.Comment;
 
 import java.util.List;
-
+/**
+ * Copyright (c) 2008-2024: Zirui Qiao
+ * Project: NART
+ * @className: CommentService
+ * @Description: This method can search the corresponding comments according to the ID of status.
+ * This method can also upload new comments.
+ *
+ * @version: v1.17.0
+ * @author: YUNZHOU LIU
+ * @date: 2023-1-14
+ *
+ */
 public interface CommentService {
 
     List<Comment> showCommentList(String statusId);
